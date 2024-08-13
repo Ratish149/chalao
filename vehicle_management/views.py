5ee81953b26e3f7871c92375bad8f0bc0f5088d8
Ratish149 <ratish.shakya149@gmail.com>
2024-08-13 17:04:12 +0545

    add/list vehicle

--- vehicle_management/views.py
+++ vehicle_management/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView
+from .models import Vehicle
+from .serializers import VehicleSerializer
 # Create your views here.
+
+class VehicleListCreateView(ListCreateAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
