942014553f97505f6b40ad6c8923a1665f5b529f
Ratish149 <ratish.shakya149@gmail.com>
2024-08-14 13:33:37 +0545

    token verification / add vehicle

--- vehicle_management/views.py
+++ vehicle_management/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+
 from .models import Vehicle
 from .serializers import VehicleSerializer
 # Create your views here.
@@ -7,6 +9,52 @@ from .serializers import VehicleSerializer
 class VehicleListCreateView(ListCreateAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    
+    def create(self, request, *args, **kwargs):
+        vendor = request.user
+        vehicle_name = request.data.get('vehicle_name')
+        vehicle_type = request.data.get('vehicle_type')
+        vehicle_image_front = request.data.get('vehicle_image_front')  
+        vehicle_image_back = request.data.get('vehicle_image_back')
+        vehicle_image_left = request.data.get('vehicle_image_left')
+        vehicle_image_right = request.data.get('vehicle_image_right')
+        vehicle_image_speedometer = request.data.get('vehicle_image_speedometer')
+        price_per_day = request.data.get('price_per_day')
+        price_per_week = request.data.get('price_per_week')
+        price_per_month = request.data.get('price_per_month')
+        bike_condition = request.data.get('bike_condition')
+        category = request.data.get('category')
+        theft_assurance = request.data.get('theft_assurance')
+        distance_travelled = request.data.get('distance_travelled')
+        last_service_date = request.data.get('last_service_date')
+        power = request.data.get('power')
+        duration = request.data.get('duration')
+        discount = request.data.get('discount')
+        
+        vehicle=Vehicle.objects.create(
+            vendor=request.user,  
+            vehicle_name=vehicle_name,
+            vehicle_type=vehicle_type,
+            vehicle_image_front=vehicle_image_front,
+            vehicle_image_back=vehicle_image_back,
+            vehicle_image_left=vehicle_image_left,
+            vehicle_image_right=vehicle_image_right,
+            vehicle_image_speedometer=vehicle_image_speedometer,
+            price_per_day=price_per_day,
+            price_per_week=price_per_week,
+            price_per_month=price_per_month,
+            bike_condition=bike_condition,
+            category=category,
+            theft_assurance=theft_assurance,
+            distance_travelled=distance_travelled,
+            last_service_date=last_service_date,
+            power=power,
+            duration=duration,
+            discount=discount
+
+        )
+        vehicle.save()
+        return Response({'Message':'Vehicle Added'})
 
 class VehicleEditView(RetrieveUpdateDestroyAPIView):
     queryset = Vehicle.objects.all()
