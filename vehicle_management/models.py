8abb86d522fe71c988d1262b6ef478b2703abbc9
Ratish149 <ratish.shakya149@gmail.com>
2024-08-14 12:12:01 +0545

    model updated

--- vehicle_management/models.py
+++ vehicle_management/models.py
@@ -19,6 +19,9 @@ class Vehicle(models.Model):
         'COVERED':'COVERED',
         'NOT COVERED':'NOT COVERED',
     }
+    CATEGORY={
+        'BUDGET':'BUDGET',
+    }
 
     vendor = models.ForeignKey('authentication.User',on_delete=models.CASCADE)
     vehicle_name = models.CharField(max_length=100)
@@ -35,7 +38,7 @@ class Vehicle(models.Model):
     price_per_month = models.IntegerField(blank=True,null=True)
 
     bike_condition=models.CharField(max_length=100,blank=True,null=True)
-    category=models.CharField(max_length=100,blank=True,null=True)
+    category=models.CharField(max_length=100,choices=CATEGORY,blank=True,null=True)
     theft_assurance=models.CharField(max_length=100,choices=THEFT_ASSURANCE)
     distance_travelled=models.IntegerField(blank=True,null=True)
     last_service_date=models.DateField(blank=True,null=True)
