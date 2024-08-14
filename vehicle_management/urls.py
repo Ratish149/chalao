90f4cf51b7591905feae211245ed2bfe8d52cf3d
Ratish149 <ratish.shakya149@gmail.com>
2024-08-14 12:08:19 +0545

    vehicle update

--- vehicle_management/urls.py
+++ vehicle_management/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('vehicle/', VehicleListCreateView.as_view(), name='vehicle'),
+    path('vehicle/<int:pk>/', VehicleEditView.as_view(), name='vehicle-edit'),
+
 ]
--- vehicle_management/views.py
+++ vehicle_management/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from .models import Vehicle
 from .serializers import VehicleSerializer
 # Create your views here.
 
 class VehicleListCreateView(ListCreateAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+class VehicleEditView(RetrieveUpdateDestroyAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
