386f8db85a7b0754e35c2437ba9f8f36f00c6e95
Ratish149 <ratish.shakya149@gmail.com>
2024-08-16 15:49:06 +0545

    admin tabularinline

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 from .models import *
 from unfold.admin import ModelAdmin
 # Register your models here.
+from django.contrib import admin
+from unfold.admin import ModelAdmin as UnfoldModelAdmin
+from .models import User, UserProfile, VendorProfile
 
-admin.site.register(User,ModelAdmin)
+class UserProfileInline( admin.TabularInline):
+    model = UserProfile
+    extra = 1
+
+class VendorProfileInline( admin.TabularInline):
+    model = VendorProfile
+    extra = 1
+
+class UserAdmin(UnfoldModelAdmin, admin.ModelAdmin):
+    inlines = []
+
+    def get_inline_instances(self, request, obj=None):
+        inline_instances = []
+        if obj:
+            if obj.user_type == 'VENDOR':
+                inline_instances.append(VendorProfileInline(self.model, self.admin_site))
+            elif obj.user_type == 'USER':
+                inline_instances.append(UserProfileInline(self.model, self.admin_site))
+        return inline_instances
+
+admin.site.register(User, UserAdmin)
 admin.site.register(UserProfile,ModelAdmin)
 admin.site.register(VendorProfile,ModelAdmin)
