7bb1684d08f1e0877b475cedb33ce22fe61c6c3e
Ratish149 <ratish.shakya149@gmail.com>
2024-08-18 15:49:21 +0545

    cors header

--- chalau/settings.py
+++ chalau/settings.py
@@ -45,11 +45,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'vehicle_management',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -148,9 +150,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[]
+STATIC_ROOT=Path(BASE_DIR,'static/')
 
 MEDIA_ROOT=BASE_DIR/'media'
 MEDIA_URL='/media/'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
