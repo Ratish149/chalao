9744df1f9c68231cd604bb2da651b44b4ce594b7
Ratish149 <ratish.shakya149@gmail.com>
2024-08-29 12:24:19 +0545

    commit

--- chalau/settings.py
+++ chalau/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-+(5!1qyxpk=^s(^p_4_y-(q=uoy$jnqp_)r1p(@j(#4t!806
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
