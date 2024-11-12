ecf58ba13d6c93b2baa4a7c78f1dafac9406b428
Ratish149 <ratish.shakya149@gmail.com>
2024-11-12 04:18:44 +0000

    python anywhere

--- chalau/settings.py
+++ chalau/settings.py
@@ -148,8 +148,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
-MEDIA_ROOT=BASE_DIR/'media'
-MEDIA_URL='/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
